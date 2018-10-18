d8c9be8bbe2886867b4487ddde6a53ff2f6b0db2
Babyis <44205044+Babyis@users.noreply.github.com>
2018-10-18 13:35:24 +0100

    Added background music and pygame

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from rooms import rooms_id
 import player
 import game_functions as gf    # all functions are referred as 'gf' now
 import game_content as gc    # all dialogue are referred as 'gc' now
+import pygame
 
 # These variables are to be global
 current_room = rooms_id['lobby']
@@ -11,6 +12,11 @@ current_dialogue = gc.opening_dialogue['description']
 # all game loops and events are tested and ran here
 # main game loop
 def game():
+    print(items.item_note['Title'])
+    pygame.mixer.init()
+    pygame.mixer.music.load("8bit.ogg")
+    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.play()
     while True:
         # this is just a test to see if this function can run
         gf.print_dialogue(current_dialogue)
