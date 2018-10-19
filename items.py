eb222ce381221df8ad2fea6de1b35e6ea3a0f674
Idris Khan <43910238+Tabsdrisbidmamul@users.noreply.github.com>
2018-10-19 11:26:09 +0100

    Added the keys

--- items.py
+++ items.py
@@ -1,44 +1,93 @@
 item_title = {
 
-    'Title' :
-     """
-                    ____________________________________
-                    \______   \   \_   _____/\__    ___/
-                     |       _/   ||    __)    |    |   
-                     |    |   \   ||     \     |    |   
-                     |____|_  /___|\___  /     |____| TM
-                            \/         \/  ADVENTURE GAME""",
+    'Title':
+        """
+                       ____________________________________
+                       \______   \   \_   _____/\__    ___/
+                        |       _/   ||    __)    |    |   
+                        |    |   \   ||     \     |    |   
+                        |____|_  /___|\___  /     |____| TM
+                               \/         \/  ADVENTURE GAME""",
 
-    'Instructions' : 
-    """
-                    ____________________________________
-                   |                                    |
-                   |            INSTRUCTIONS            |
-                   |                                    |
-                   |   TYPE "go" TO NAVIGATE THROUGH    |
-                   |               ROOMS                |
-                   |                                    |
-                   |   TYPE "take" TO PICK UP AN ITEM   |
-                   |                                    |
-                   | IN ORDER TO SOLVE THE RIDDLES YOU  |
-                   | NEED TO "interact" WITH THE OBJECT |
-                   |____________________________________|"""                                  
-                   
+    'Instructions':
+        """
+                        ____________________________________
+                       |                                    |
+                       |            INSTRUCTIONS            |
+                       |                                    |
+                       |   TYPE "go" TO NAVIGATE THROUGH    |
+                       |               ROOMS                |
+                       |                                    |
+                       |   TYPE "take" TO PICK UP AN ITEM   |
+                       |                                    |
+                       | IN ORDER TO SOLVE THE RIDDLES YOU  |
+                       | NEED TO "interact" WITH THE OBJECT |
+                       |____________________________________|""",
 
 }
 
-item_note1= {
+item_note1 = {
     'id': 'note',
 
-    'name': 'note',        
+    'name': 'note',
 
     'riddle_1': 'placeholder_riddle_1',
 
     'riddle_2': 'placeholder_riddle_2',
 
     'riddle_3': 'placeholder_riddle_3',
-    
+
     'riddle_4': 'placeholder_riddle_4',
+
+}
+
+item_key_1 = {
+    'name': 'key for main door',
+
+    'description': 'This is is 1 of 4 keys that will unlock the main door',
+
+}
+
+item_key_2 = {
+    'name': 'key for main door',
+
+    'description': 'This is is 1 of 4 keys that will unlock the main door',
+
+}
+
+item_key_3 = {
+    'name': 'key for main door',
+
+    'description': 'This is is 1 of 4 keys that will unlock the main door',
+
+}
+
+item_key_4 = {
+    'name': 'key for main door',
+
+    'description': 'This is is 1 of 4 keys that will unlock the main door',
+
+}
+
+
+item_riddle_candle = {
+    'name': 'candle',
+
+    'description': 'A lone candle stands in solitary darkness.',
+
+    'description_2': 'You set the candle alight, the wax melts away to '
+                     'reveal a key lodged within it.',
+
+    'on': False,
+
+}
+
+item_matchsticks = {
+    'name': 'matchsticks',
+
+    'description': 'A box of matchsticks, there are only 3 matchsticks left '
+                   'in the box. Maybe I could use them to set something '
+                   'alight.',
 
 }
 
