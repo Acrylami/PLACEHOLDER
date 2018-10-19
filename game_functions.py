faac3c73e05e83239575d706f0b16704127b5108
octavioloffaro <44145092+octavioloffaro@users.noreply.github.com>
2018-10-19 14:32:35 +0100

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -198,41 +198,7 @@ def riddle_1():
     x = input("I'm tall when I'm young and I'm short when I'm old. What am I?")
     if x == "candle" or "a candle":
         pass
-"""    
-def take():
-    #take key and display:
     
-    print("")
-    print("                      :sdmNNNds/`                                                                           ")
-    print("                    -dMNho//oyNMm/                                                                          ")
-    print("                   -NMd-      .yMM/                                                                         ")
-    print("                   oMM:        `MMh                                                                         ")
-    print("             `:oyhhmMMs        :MMy                                                                         ")
-    print("           `omMNdhhmMMm:    .-sNMd--`.oo:                                                                   ")
-    print("           oMMh-   `:+.`sdyomMMMNdNMdmMMMoooooooooooo++++++oooooooooooooooooooooooooooooooooooooooo+-oso.   ")
-    print("           dMM-        /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMh   ")
-    print("           +MMh-`  `:+.`sdyomMMMNdNMdmMMMooooooooooooooooooooosssssssssssssssssssssssssdMMMMMMmyssys/yhs-   ")
-    print("            +mMNdhhmMMm:    .-sNMd--`.oo:                                         yyyyydMMMMMMmyyyyy`       ")
-    print("             `:oyyymMMo        /MMs                                              `dNMMMMMMMMMMMMMMNd`       ")
-    print("                   sMM:        `MMh                                               /dMMMMMMMMMMMMMMd+`       ")
-    print("                   -NMd-      .hMM/                                              `MMMMNyyMMMMyyNMMMM.       ")
-    print("                    -dMNho//ohNMm/                                               `MMMMm  NMMM  dMMMM.       ")
-    print("                      :sdmNNNds/`                                                `mmmmh  dmmm  ymmmm`       ")
-    print("")
-    print("    ")
-    print("   ")
-    print("   ")
-    print("                                                       ")                                        
-    print("                                                        ")                                       
-    print("        `:`   :`  -/+/-   --    :`      `://:`   .://-  .//////.     `:.      `:   :` :////`--   --  s.   ")  
-    print("         od` sy .do-.-sd` sy    N:    `yy:..:/  yh-..+m-`.-yh..`    +om+      /m -h+  mo--- -m: :m.  N-    ") 
-    print("          +dsy  sh     m/ sy    N:    +d  .--- :N`    sh   sy         d+      /Nsy`   my++:  .m+m.   N-   ")  
-    print("           sd   sh     N/ sy    N:    +m  .-od :N`    yy   sy         d+      /m:d/   m+``    -M-    m.    ") 
-    print("           oh   .ds:-/hs  -m+-:sh      sh+::yd  sh/-:sh`   sy       -:ds:`    /m `sh. ms:::`  .M`   `o.     ")
-    print("           `.     .::-`     -::`         -::.    `-:-`     ..       -----`    `.   .. .----`   -     :`   ")  
-        
-
-"""
 
 
 def print_menu():
