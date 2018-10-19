2db82088f946550db41efcd01392f00ac50b78f7
Babyis <44205044+Babyis@users.noreply.github.com>
2018-10-19 14:22:15 +0100

    Update game_content.py

--- game_content.py
+++ game_content.py
@@ -1,4 +1,7 @@
+import items
 # contain all the storyline/ dialogue
+
+current_riddle = items.item_title['Instructions']
 
 # Will contain the opening plot information
 opening_dialogue = {
@@ -9,5 +12,12 @@ opening_dialogue = {
 floor, staring up at a dimly lit chandelier covered in cobwebs on the 
 ceiling. Next to you is a heavy locked door. There are 4 keyholes. 
 How did you get here? You must escape.""",
+}
+
+key1_dialogue = {
+    "description":
+
+    """You have obtained ONE of the FOUR keys to unlock the main door.
+                You now have access to the NURSERY."""
 }
 
