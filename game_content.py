3a0bd55b18f35db768659abbaf8ac86a541d07ec
Babyis <44205044+Babyis@users.noreply.github.com>
2018-10-19 10:14:02 +0100

    Update game_content.py

--- game_content.py
+++ game_content.py
@@ -5,7 +5,7 @@ opening_dialogue = {
     "description":
 
     # all dialogue will follow this layout to outputted properly
-    """You wake up lying in a pool of blood on a hard wood 
+    """You wake up lying in a pool of blood on a hard wooden 
 floor, staring up at a dimly lit chandelier covered in cobwebs on the 
 ceiling. Next to you is a heavy locked door. There are 4 keyholes. 
 How did you get here? You must escape.""",
