962c83b8e544253a742db0c0b58d78417a130520
Idris Khan <43910238+Tabsdrisbidmamul@users.noreply.github.com>
2018-10-22 15:00:08 +0100

    Update game_content.py

--- game_content.py
+++ game_content.py
@@ -8,10 +8,10 @@ opening_dialogue = {
     "description":
 
     # all dialogue will follow this layout to outputted properly
-    """You wake up lying in a pool of blood on a hard wooden 
-floor, staring up at a dimly lit chandelier covered in cobwebs on the 
-ceiling. Next to you is a heavy locked door. There are 4 keyholes. 
-How did you get here? You must escape.""",
+    """\n\t\t\t\t\t\tYou wake up lying in a pool of blood on a hard wooden 
+\t\t\t\t\t\tfloor, staring up at a dimly lit chandelier covered in cobwebs on the 
+\t\t\t\t\t\tceiling. Next to you is a heavy locked door. There are 4 keyholes. 
+\t\t\t\t\t\tHow did you get here? You must escape.""",
 }
 
 key1_dialogue = {
@@ -21,4 +21,3 @@ key1_dialogue = {
     You have obtained ONE of the FOUR keys to unlock the main door.
     You now have access to the NURSERY."""
 }
-
