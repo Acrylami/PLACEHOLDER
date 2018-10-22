4d7e8ce95ec38708211051288fc690f165bc1ee4
Babyis <44205044+Babyis@users.noreply.github.com>
2018-10-22 11:20:20 +0100

    Update player.py

--- player.py
+++ player.py
@@ -1,4 +1,8 @@
 # all player details are placed in this file
 import items
-last_output = ''
 inventory = [ items.item2 ]
+key_counter = 0
+has_printed_1 = False
+has_printed_2 = False
+has_printed_3 = False
+has_printed_4 = False
