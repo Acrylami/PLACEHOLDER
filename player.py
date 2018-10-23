f4c17b71b6850d29c64319fb8b887dc8c3caf379
Babyis <44205044+Babyis@users.noreply.github.com>
2018-10-23 16:27:03 +0100

    Update player.py

--- player.py
+++ player.py
@@ -1,7 +1,6 @@
 # all player details are placed in this file
 import items
-inventory = [items.item_note1]
-key_counter = 0
+inventory = []
 has_printed_1 = False
 has_printed_2 = False
 has_printed_3 = False
